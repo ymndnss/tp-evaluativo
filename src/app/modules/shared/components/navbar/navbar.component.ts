1704bc6acba717c033bf9ce0301272d0cf3b3f18
pedrocv66 <cuevasp931@gmail.com>
2024-07-26 10:41:12 -0300

    mejorando css

--- src/app/modules/shared/components/navbar/navbar.component.ts
+++ src/app/modules/shared/components/navbar/navbar.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/modules/autentificacion/services/auth.service';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent {
+  logueado = true;
+  deslogueado = false;
+  constructor(public servicioAuth: AuthService, public servicioRutas: Router) {}
 
+  iniciar() {
+    (this.logueado = false), (this.deslogueado = true);
+  }
+  cerrarSesion() {
+    (this.deslogueado = false), this.servicioAuth.cerrarSesion();
+
+    this.servicioRutas.navigate(['/']); //redirigimos a la raiz de la pagina
+    this.logueado = true;
+  }
 }
